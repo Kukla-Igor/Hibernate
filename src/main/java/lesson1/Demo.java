65f335f5bf93f2037fa298f2e1f76eb41dd21344
igor.kukla1992@gmail.com <170892igor>
2019-04-02 14:45:53 +0300

    Initial commit

---
+++ src/main/java/lesson1/Demo.java
@@ -0,0 +1,30 @@
+package lesson1;
+
+import org.hibernate.Session;
+
+public class Demo {
+    public static void main(String[] args) {
+        save();
+
+    }
+
+    static void save(){
+        Session session = new HibernateUtils().createSessionFactory().openSession();
+
+        session.getTransaction().begin();
+
+        Product product = new Product();
+        product.setId(99);
+        product.setName("Table");
+        product.setDescription("grey & blue");
+        product.setPrice(70);
+        session.save(product);
+
+        session.getTransaction().commit();
+
+        System.out.println("Done");
+
+        session.close();
+
+    }
+}
---
+++ src/main/java/lesson1/Product.java
@@ -0,0 +1,53 @@
+package lesson1;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+
+
+    @Id
+    @Column(name = "ID")
+    public long getId() {
+        return id;
+    }
+
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    @Column(name = "PRICE")
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
